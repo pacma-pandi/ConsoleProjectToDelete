77af967c8e4d91fd01b3f8517bacbb01925d779e
bou8 <konstantinosbou@yahoo.gr>
2020-07-21 14:16:22 +0300

    First commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Person p = new Person();
+        p.setName("Thomas");
+
+        System.out.println("Hello " + p.getName());
+    }
+
+
+}
